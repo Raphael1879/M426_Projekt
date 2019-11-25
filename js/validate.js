f56660efa11604c98cd55df506cfb18598947059
Stamm Raphael <raphael.stamm@wiss-online.ch>
2019-11-25 16:13:44 +0100

    added form validation

--- js/validate.js
+++ js/validate.js
@@ -1,29 +1,41 @@
-$("#myform").validate({
+$.validator.addMethod("checkPasswordComplexity", function(value, element)
+{
+  var hasDigits = /[0-9]/.test(value);
+  var hasCaps = /[A-Z]/.test(value);
+  return hasDigits && hasCaps;
+
+},
+"Passworts muss mindestens eine Zahl und Grossbuchstaben enthalten" //Auszugebende Message falls nicht erfüllt.
+);
+
+$("#form").validate({
   rules:
   {
     inputUsername:
     {
-
+      required: true,
+      minlength: 3
     },
 
     inputEmail:
     {
-
+      required: true,
+      email: true
     },
 
     inputRole:
     {
-
+      required: true,
     },
 
     inputPassword:
     {
-
+      required: true
     },
 
     inputConfirmPassword:
     {
-
+      equalTo: inputPassword
     }
 
   },
