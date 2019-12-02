e5f3b43b39aae4867314f79a9d5f50b8c758e95f
Singh Gurpreet <gurpreet.singh@wiss-online.ch>
2019-12-02 08:51:52 +0100

    updated register

--- login.php
+++ login.php
@@ -4,7 +4,7 @@ $benutzername = $_GET['inputUsername'];
 $password = $_GET['inputPassword'];
 
 
-if (isset($_GET['inputUsername']) && isset($_GET['inputPassword'])) {
+if (isset($benutzername) && isset($password)) {
 
   echo "Hallo " . $benutzername . " dein Passwort lautet " . $password;
 }
