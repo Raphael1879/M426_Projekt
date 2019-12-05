a9b9de6b1c7cffef6035dc18939609264cf291c1
funbreak <55190405+funbreak@users.noreply.github.com>
2019-12-05 11:28:20 +0100

    Updated Register Screen

--- footer.php
+++ footer.php
@@ -1,3 +1,13 @@
+<?php
+session_start ();
+$benutzername = $_GET['inputUsername'];
+$password = $_GET['inputPassword'];
+$rolle = $_GET['inputRole'];
+$_SESSION['inputUsername'] = $benutzername;
+$_SESSION['inputPassword'] = $password;
+$_SESSION['inputRole'] = $rolle;
+?>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -5,22 +15,19 @@
     <title>Login</title>
 
     <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/styles.css">
   </head>
   <body>
     <header class="container">
       <div class="jumbotron">
         <h1>Benutzerinformationen</h1>
         <article>
-           <?php echo "<h4> Ihr Benutzername ist: " . $_SESSION['inputUsername'] . "<br>" . "Ihr Passwort ist: " . $_SESSION['inputPassword'] . "</h4>";
-                 echo "<h4> Sie sind ein: " . $_SESSION['inputRole'] . "</h4>";?>
+          <?php echo "<h4> Ihr Benutzername ist: " . $_SESSION['inputUsername'] . "<br>" . "Ihr Passwort ist: " . $_SESSION['inputPassword'] . "</h4>";
+                echo "<h4> Sie sind ein: " . $_SESSION['inputRole'] . "</h4>";?>
         </article>
       </div>
     </header>
 
 
-    <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.1/jquery.validate.js"></script>
   </body>
 </html>
